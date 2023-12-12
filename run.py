e2a542bec4f41b74caa61571e19b4f2e638422be
vikamvorobyeva <147232540+vikamvorobyeva@users.noreply.github.com>
2023-12-12 19:53:17 +0300

    Update run.py

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     st.write('''This project is made by Vorobeva Viktoriya group (231-2). \nThis bot can make graphics connected to my DATA-set (Fruits and Vegetables Prices In USA In The Year 2020), can show some interesting statistics and check hypotisis. \nThis dataset contains information about the 'Fruits and Vegetables Prices In USA In The Year 2020'. The dataset contains 8 columns and 156 rows.\nThe column description of the dataset is as follows:\n1) Item: Name of the fruit or the vegetable.\n2) Form: The form of the item, i.e., canned, fresh, juice, dried or frozen.\n3) Retail Price: Average retail price of the item in the year.\n4) Retail Price Unit: Average retail price's measurement unit.\n5) Yield: Average yield of the item in the year.\n6) Cup Equivalent Size: Comparison done with one edible cup of food.\n7) Cup Equivalent Unit: Comparison's measurement unit.\n8) Cup Equivalent Price: Price per edible cup equivalent (The Unit of Measurement for Federal Recommendations for Fruit and Vegetable Consumptions''')
     st.dataframe(data.head())
     st.subheader("Graphics")
-    choose = st.selectbox("Graphics: ", ['interrelation (Difference to Price In %) and (Difference to Cup In %)  without Form Juice', 'describe', ' Proportion of Fresh, Canned, etc... Fruits and Vegetables',111'Comparison Cup equivalent price with its size' ' interrelation (Difference to Price In %) and (Difference to Cup In %)  without Form Juice',' Graphic Fresh',' Graphic Canned',  ' Graphic Juice',  ' Graphic Dried',' Graphic Frozen', ])
+    choose = st.selectbox("Graphics: ", ['interrelation (Difference to Price In %) and (Difference to Cup In %)  without Form Juice', 'describe', ' Proportion of Fresh, Canned, etc... Fruits and Vegetables','Connection between different Forms and Retail price',1111 ' interrelation (Difference to Price In %) and (Difference to Cup In %)  without Form Juice',' Graphic Fresh',' Graphic Canned',  ' Graphic Juice',  ' Graphic Dried',' Graphic Frozen', ])
     if choose == 'interrelation (Difference to Price In %) and (Difference to Cup In %)  without Form Juice':
         response = requests.get(https://raw.githubusercontent.com/vikamvorobyeva/HSE_BOT/main/cupsize.png)
         img =
@@ -23,8 +23,8 @@ if __name__ == "__main__":
     if choose == ' Proportion of Fresh, Canned, etc... Fruits and Vegetables':
         response = requests.get(https://raw.githubusercontent.com/vikamvorobyeva/HSE_BOT/main/different_forms.png)
         img =
-    if choose == 'Comparison Cup equivalent price with its size':
-        response = requests.get()
+    if choose == 'Connection between different Forms and Retail price':
+        response = requests.get(https://raw.githubusercontent.com/vikamvorobyeva/HSE_BOT/main/forms_from_retailprice.png)
         img =
     if choose == 'interrelation (Difference to Price In %) and (Difference to Cup In %) without Form Juice':
         response = requests.get()
